3d8133b48b856f95436f390e5dfba26745c46f90
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-12-25 15:20:05 +0800

    Create putConvertToPdfWithPassword.py

---
+++ Python Word examples/convert/putConvertToPdfWithPassword.py
@@ -0,0 +1,17 @@
+import spirecloudword
+from spirecloudword.configuration import Configuration as WordConfiguration
+
+appId = "your id"
+appKey = "your key"
+
+configuration = WordConfiguration(appId, appKey)
+api = spirecloudword.api.convert_api.ConvertApi(configuration)
+file_name = "Encrypt.docx"
+target_format = "pdf"
+inputFolder = "input"
+outputFilePath = "output/ConvertToPdfWithPassword.pdf"
+password = "123"
+result = api.convert_document(file_name, target_format, folder=inputFolder, dest_file_path=outputFilePath, password=password)
+
+
+
