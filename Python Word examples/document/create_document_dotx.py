7a7e259c9b8434616afb874b1e5292b0a109dc93
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-12-25 15:20:30 +0800

    Create create_document_dotx.py

---
+++ Python Word examples/document/create_document_dotx.py
@@ -0,0 +1,14 @@
+import spirecloudword
+from spirecloudword.configuration import Configuration as WordConfiguration
+
+appId = "your id"
+appKey = "your key"
+
+configuration = WordConfiguration(appId, appKey)
+api = spirecloudword.api.word_document_api.WordDocumentApi(configuration)
+
+outputPath = "output/WordDocument"
+file_format = "dotx"
+filename = "create_document_dotx"
+result = api.create_document(file_format, filename, dest_folder=outputPath)
+
