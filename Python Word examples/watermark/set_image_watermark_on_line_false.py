8fce27cb4a3cea10bc49f71ae14ec35e1a405e3f
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-12-25 15:23:45 +0800

    Create set_image_watermark_on_line_false.py

---
+++ Python Word examples/watermark/set_image_watermark_on_line_false.py
@@ -0,0 +1,22 @@
+import spirecloudword
+from spirecloudword.configuration import Configuration as WordConfiguration
+
+appId = "your id"
+appKey = "your key"
+
+configuration = WordConfiguration(appId, appKey)
+api = spirecloudword.api.watermarks_api.WatermarksApi(configuration)
+
+filename = "Template.docx"
+input_image = "logo.png"
+inputFolder = "input"
+outputPath = "output/Watermark/set_image_watermark_on_line_false.docx"
+
+extra = {"folder": inputFolder,
+         "scaling": 120,
+         "washout": False,
+         "dest_file_path": outputPath}
+
+result = api.set_image_watermark_on_line(filename, input_image=input_image, **extra)
+
+
