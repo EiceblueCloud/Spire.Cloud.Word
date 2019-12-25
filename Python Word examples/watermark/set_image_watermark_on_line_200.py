66bc452b523f2234e7e8294baac317afa7992d32
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-12-25 15:23:43 +0800

    Create set_image_watermark_on_line_200.py

---
+++ Python Word examples/watermark/set_image_watermark_on_line_200.py
@@ -0,0 +1,21 @@
+import spirecloudword
+from spirecloudword.configuration import Configuration as WordConfiguration
+
+appId = "your id"
+appKey = "your key"
+
+configuration = WordConfiguration(appId, appKey)
+api = spirecloudword.api.watermarks_api.WatermarksApi(configuration)
+
+filename = "Encrypt.docx"
+inputFolder = "input"
+password = "123"
+outputPath = "output/Watermark/set_image_watermark_on_line_Pw_200.docx"
+
+extra = {"folder": inputFolder,
+         "scaling": 200,
+         "washout": True,
+         "password": password,
+         "dest_file_path": outputPath}
+
+result = api.set_image_watermark_on_line(filename, input_image="logo.png", **extra)
