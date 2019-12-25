a613859da0179a4a208c8b17f881fc78ca11b60e
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-12-25 15:20:16 +0800

    Create putConvertToXpsWithPassword.py

---
+++ Python Word examples/convert/putConvertToXpsWithPassword.py
@@ -0,0 +1,17 @@
+import spirecloudword
+from spirecloudword.configuration import Configuration as WordConfiguration
+
+appId = "your id"
+appKey = "your key"
+
+configuration = WordConfiguration(appId, appKey)
+api = spirecloudword.api.convert_api.ConvertApi(configuration)
+file_name = "Encrypt.docx"
+target_format = "xps"
+inputFolder = "input"
+outputFilePath = "output/ConvertToXpsWithPassword.xps"
+password = "123"
+result = api.convert_document(file_name, target_format, folder=inputFolder, dest_file_path=outputFilePath, password=password)
+
+
+
