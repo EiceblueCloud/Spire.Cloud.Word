c6cf4b824cb12766b0b7e07ed999409a83b96e77
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-12-25 15:19:47 +0800

    Create putConvertToDocx.py

---
+++ Python Word examples/convert/putConvertToDocx.py
@@ -0,0 +1,17 @@
+import spirecloudword
+from spirecloudword.configuration import Configuration as WordConfiguration
+
+appId = "your id"
+appKey = "your key"
+
+configuration = WordConfiguration(appId, appKey)
+api = spirecloudword.api.convert_api.ConvertApi(configuration)
+
+file_name = "Template.docx"
+target_format = "docx"
+inputFolder = "input"
+outputFilePath = "output/ConvertToDocx.docx"
+result = api.convert_document(file_name, target_format, folder=inputFolder, dest_file_path=outputFilePath)
+
+
+
